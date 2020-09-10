cf8f1eac328f289a4b7ef0d1ba9bded06ab46e20
jessice <jessicesun@gmail.com>
2020-09-10 11:46:45 +0800

    at

---
+++ app/src/main/java/com/example/sun29/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.sun29
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
